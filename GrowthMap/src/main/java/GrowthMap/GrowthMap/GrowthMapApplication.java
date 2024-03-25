e6748381147946b5a91501849b89f2aacf250fe7
JunHyeokChoi1 <118219739+JunHyeokChoi1@users.noreply.github.com>
2024-03-25 23:13:28 +0900

    스프링부트 프로젝트 생성 3/25

---
+++ GrowthMap/src/main/java/GrowthMap/GrowthMap/GrowthMapApplication.java
@@ -0,0 +1,13 @@
+package GrowthMap.GrowthMap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrowthMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrowthMapApplication.class, args);
+	}
+
+}
---
+++ GrowthMap/src/test/java/GrowthMap/GrowthMap/GrowthMapApplicationTests.java
@@ -0,0 +1,13 @@
+package GrowthMap.GrowthMap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrowthMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
